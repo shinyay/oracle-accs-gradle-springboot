4bb457961607756590f1b715c2197fcd48ac23bb
shinyay <shinya.com@gmail.com>
2017-12-22 18:57:22 +0900

    Modify Request Mapping

--- src/main/kotlin/com/shinyay/sample/springboot/demokotlin/controller/MainController.kt
+++ src/main/kotlin/com/shinyay/sample/springboot/demokotlin/controller/MainController.kt
@@ -2,13 +2,14 @@ package com.shinyay.sample.springboot.demokotlin.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import com.shinyay.sample.springboot.demokotlin.model.fizzBuzzLambda
 
 @Controller
 class MainController {
-    @RequestMapping
+    @GetMapping("/")
     fun index(model: Model): String {
-        val fizzbuzz = com.shinyay.sample.springboot.demokotlin.model.fizzBuzzLambda(50)
+        val fizzbuzz = fizzBuzzLambda(50)
         model.addAttribute("message", fizzbuzz)
         return "index"
     }
